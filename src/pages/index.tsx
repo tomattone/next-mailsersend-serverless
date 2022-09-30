1e8c6b8fb14c121f5f86c535049c96cea80c70f8
Armando Tomazzoni Jr <armando.junior@luizalabs.com>
2022-09-30 02:10:30 -0300

    💅 design homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <h1>E-mail Serveless API</h1>
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontFamily: 'sans-serif',
+      }}
+    >
+      <h1>E-mail Serverless API</h1>
+    </div>
+  )
 }
 
 export default Home
